b3267c1edf6c514ac5267f04596a3d74cb2fb7d3
Tino Sabetta <tino.sabetta@gmail.com>
2022-03-22 15:26:54 +0100

    made script importable

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 /* --------------------------------------------------------------------------------------------------
-Imports & data
+Import
+---------------------------------------------------------------------------------------------------*/
+import {renderTemplate} from './renderTemplate.js';
+
+/* --------------------------------------------------------------------------------------------------
+Data
 ---------------------------------------------------------------------------------------------------*/
 
 var persons = {
@@ -29,93 +34,11 @@ var persons = {
 /* --------------------------------------------------------------------------------------------------
 Variables
 ---------------------------------------------------------------------------------------------------*/
-
+var personTemplate = document.querySelector("#persons");
+var appendHere = document.querySelector("#here");
 
 /* --------------------------------------------------------------------------------------------------
 functions
 ---------------------------------------------------------------------------------------------------*/
 
-// transform a string into properties of an object with dot-notation
-function chainProps(obj, props) {
-    if (!props)
-        return obj;
-    var propsArr = props.split('.');
-    var prop = propsArr.splice(0, 1);
-    return chainProps(obj[prop], propsArr.join('.'));
-}
-
-function renderTemplate(template, data, domEl) {
-    var clone = template.content.cloneNode(true);
-
-    //Loops
-    var loops = clone.querySelectorAll("[data-loop]");
-    for (let i = 0; i < loops.length; i++) {
-		loops[i].dataset.templateIteration = 0;
-
-        let loopData = loops[i].dataset.loop;
-        for (let j = 1; j < data[loopData].length; j++) {
-			var clonedLoop = loops[i].cloneNode(true);
-			clonedLoop.dataset.templateIteration = data[loopData].length-j;
-            loops[i].after(clonedLoop);
-        }
-
-    }
-
-    loops = clone.querySelectorAll("[data-loop]");
-    for (let i = 0; i < loops.length; i++) {
-        let loopData = loops[i].dataset.loop;
-        let loopVars = loops[i].querySelectorAll("var");
-
-        for (let j = 0; j < loopVars.length; j++) {
-            if (loopVars[j].textContent === "") {
-                // TO DO: Iterate over variables and append index
-                loopVars[j].textContent = loopData + "." + loops[i].dataset.templateIteration;
-            }
-            else {
-                loopVars[j].textContent = loopData + "." + loops[i].dataset.templateIteration + "." + loopVars[j].textContent;
-            }
-        }
-        loops[i].outerHTML = loops[i].innerHTML;
-    }
-
-    //Attributes
-    var templateAttr = clone.querySelectorAll("[data-attr]");
-
-    for (let values of templateAttr) {
-        let attr = values.dataset.attr.split(":");
-        let key = attr[0];
-        let value = chainProps(data, attr[1]);
-        values.setAttribute(key, value);
-        values.removeAttribute("data-attr");
-        console.log(values.attributes);
-    }
-
-    //Variables
-    var templateVars = clone.querySelectorAll("var");
-    for (let values of templateVars) {
-        values.innerHTML = chainProps(data, values.textContent);
-        values.removeAttribute("data-var");
-        if (values.tagName === "VAR") {
-            values.outerHTML = values.innerHTML;
-        }
-    }
-
-    domEl.append(clone);
-}
-
-function init() {
-    document.addEventListener("touchstart", function() {}, false);
-
-    var personTemplate = document.querySelector("#persons");
-    var appendHere = document.querySelector("#here");
-    renderTemplate(personTemplate, persons, appendHere);
-}
-
-/* --------------------------------------------------------------------------------------------------
-public members, exposed with return statement
----------------------------------------------------------------------------------------------------*/
-window.app = {
-    init
-};
-
-app.init();
+renderTemplate(personTemplate, persons, appendHere);
---
+++ js/renderTemplate.js
@@ -0,0 +1,65 @@
+// transform a string into properties of an object with dot-notation
+function chainProps(obj, props) {
+    if (!props)
+        return obj;
+    var propsArr = props.split('.');
+    var prop = propsArr.splice(0, 1);
+    return chainProps(obj[prop], propsArr.join('.'));
+}
+
+export function renderTemplate(template, data, domEl) {
+    var clone = template.content.cloneNode(true);
+
+    //Loops
+    var loops = clone.querySelectorAll("[data-loop]");
+    for (let i = 0; i < loops.length; i++) {
+		loops[i].dataset.templateIteration = 0;
+
+        let loopData = loops[i].dataset.loop;
+        for (let j = 1; j < data[loopData].length; j++) {
+			var clonedLoop = loops[i].cloneNode(true);
+			clonedLoop.dataset.templateIteration = data[loopData].length-j;
+            loops[i].after(clonedLoop);
+        }
+    }
+
+    loops = clone.querySelectorAll("[data-loop]");
+    for (let i = 0; i < loops.length; i++) {
+        let loopData = loops[i].dataset.loop;
+        let loopVars = loops[i].querySelectorAll("var");
+
+        for (let j = 0; j < loopVars.length; j++) {
+            if (loopVars[j].textContent === "") {
+                // TO DO: Iterate over variables and append index
+                loopVars[j].textContent = loopData + "." + loops[i].dataset.templateIteration;
+            }
+            else {
+                loopVars[j].textContent = loopData + "." + loops[i].dataset.templateIteration + "." + loopVars[j].textContent;
+            }
+        }
+        loops[i].outerHTML = loops[i].innerHTML;
+    }
+
+    //Attributes
+    var templateAttr = clone.querySelectorAll("[data-attr]");
+
+    for (let values of templateAttr) {
+        let attr = values.dataset.attr.split(":");
+        let key = attr[0];
+        let value = chainProps(data, attr[1]);
+        values.setAttribute(key, value);
+        values.removeAttribute("data-attr");
+    }
+
+    //Variables
+    var templateVars = clone.querySelectorAll("var");
+    for (let values of templateVars) {
+        values.innerHTML = chainProps(data, values.textContent);
+        values.removeAttribute("data-var");
+        if (values.tagName === "VAR") {
+            values.outerHTML = values.innerHTML;
+        }
+    }
+
+    domEl.append(clone);
+}
